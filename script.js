60c98e3ee7abf421639d080bcce8f4b8c6bbe672
Abhisek Das <abhisekdas@Abhiseks-MacBook-Air.local>
2024-01-12 23:24:47 +0530

    Update

---
+++ script.js
@@ -0,0 +1,11 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+el: document. querySelector ('#main'),
+smooth: true
+}) ;
+
+let a = document. querySelector ("#elem1")
+console.log(a)
+
+let b = document. querySelector ("#elem2");
